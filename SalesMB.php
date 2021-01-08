1f56b832982d5456934ddc3b3fe3fbef17dedc59
DanMATEI <danmatei.terex@yahoo.com>
2021-01-08 12:36:45 +0200

    Add files via upload

---
+++ SalesMB.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="formatcss.css">
+</head>
+
+<body>
+
+<div class="meniu">
+<?php include 'meniuMB.php';?>
+</div>
+
+<!--<p>Net Sales, Gross margin<p>-->
+<iframe width="1557" height="741.25" src="https://app.powerbi.com/reportEmbed?reportId=ab2fb73b-d782-4960-8d8d-9e62cb89613a&autoAuth=true&ctid=ba302577-7549-40e1-99e4-bbe756f7eae5&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWUtcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+
+
+</body>
+</html
---
+++ meniuMB.php
@@ -0,0 +1,46 @@
+<!DOCType>
+<HTML>
+<head>
+	<style>
+	ul {
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+			overflow: hidden;
+			background-color: #45a049;
+		}
+
+		li {
+			float: left;
+		}
+
+		li a {
+			display: block;
+			color: white;
+			text-align: center;
+			padding: 14px 16px;
+			text-decoration: none;
+		}
+
+		/* Change the link color to #111 (black) on hover */
+		li a:hover {
+			background-color: #111;
+		}
+
+	</style>
+</head>
+<body>
+
+		<div>
+		<ul>
+				<li><a href="SalesMB.php">Net Sales, Gross Margin</a>
+				<li><a href="SalesAndMarketingSampleMB.php">Sales & Marketing</a>
+				<li><a href="SalesMB.php">Actuals vs Budget Vs Forecasts</a>
+				<li><a href="SalesMB.php">Orders & Sales</a>
+
+				<!--<li><a href="inserareVizite.php">Rapoarte medicamente</a>-->
+		<ul>
+		</div>
+
+</body>
+</HTML>
